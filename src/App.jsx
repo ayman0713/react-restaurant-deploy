395b2c20f59a86914094076caa150d5e429fba66
ayman0713 <aymansr94@gmail.com>
2025-03-13 12:04:15 +0000

    Fix Navbar import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
